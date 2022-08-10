bd6043aeabc8f8dcacfa5f61a4d397c55c3346b4
Newman22 <pyronewman2@gmail.com>
2022-08-10 15:07:01 -0500

    Update MediaWiki-Candy.php

--- MediaWiki-Candy.php
+++ MediaWiki-Candy.php
@@ -1,51 +1,22 @@
 <?php
-/*
-    Candy is a MediaWiki skin that can be used as your VERY own fairy tale resort on an URL!
 
-    Copyright (c) 2013 Newman2 <pyronewman2@gmail.com>
+if ( function_exists( 'wfLoadSkin' ) ) {
+	wfLoadSkin( 'Candy' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['Candy'] = __DIR__ . '/i18n';
+	/* wfWarn(
+		'Deprecated PHP entry point used for Candy skin. Please use wfLoadSkin instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	); */
+	return true;
+} else {
+	die( 'This version of the Candy skin requires MediaWiki 1.25+' );
+}
 
-    This file is part of Candy.
+$wgAutoloadClasses['SkinCandy']     = dirname(__FILE__).'/SkinCandy.php';
 
-    Candy is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+$wgAutoloadClasses['CandyTemplate']     = dirname(__FILE__).'/CandyTemplate.php';
 
-    Candy is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Candy. If not, see <http://www.gnu.org/licenses/>.
-*/
-
-if (!defined('MEDIAWIKI'))
-    die ('This is a skin extension to the MediaWiki package and cannot be run standalone.');
-
-$wgExtensionCredits['skin'][] = array
-(
-    'path' => __FILE__
-,   'name' => 'Candy'
-,   'version' => '0.01'
-,   'author' => '[community.wikia.com/wiki/User:James Newman Newman2]'
-,   'descriptionmsg' => 'candyskin-descmsg'
+$wgResourceModuleSkinStyles['skins.candy.styles'] = array(
+  'candy' => 'Candy/main.css',
 );
-
-$wgValidSkinNames['candy'] = 'Candy';
-
-$wgAutoloadClasses['SkinCandy']     = dirname(__FILE__).'/Candy.skin.php';
-$wgAutoloadClasses['CandyRenderer'] = dirname(__FILE__).'/Candy.renderer.php';
-$wgExtensionMessagesFiles['Candy']  = dirname(__FILE__).'/Candy.i18n.php';
-
-$wgResourceModules['skins.candy'] = array
-(
-    'styles' => array
-    (
-        'Candy/assets/css/MediaWiki.css' => array('media' => 'screen')
-    ,   'Candy/assets/css/Candy.css' => array('media' => 'screen')
-    )
-,   'remoteBasePath' => &$GLOBALS['wgStylePath']
-,   'localBasePath'  => &$GLOBALS['wgStyleDirectory']
-);
-?>
